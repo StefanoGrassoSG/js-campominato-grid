90f617386bcc56bcc94d67715bf66f1d7775afcc
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-21 15:22:24 +0200

    ADD toggle class js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ for (i = 1; i <= 100; i++) {
     const newCell = document.createElement('div');
     newCell.classList.add('square');
     myCont.append(newCell);
+    newCell.innerHTML = i;
+    newCell.addEventListener('click', function(){
+        this.classList.toggle('active')
+        console.log(this.innerHTML)
+    })
+    
 
 }
 
