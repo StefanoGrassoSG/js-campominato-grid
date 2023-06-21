bb6c1d2d66d87b3137196e7d8c6f6e04691ef287
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-21 18:47:34 +0200

    EDIT functions in js

--- js/script.js
+++ js/script.js
@@ -1,64 +1,61 @@
+const startButton = document.getElementById('start')
+const container = document.querySelector('.container-game')
 
-const myCont = document.querySelector('.container')
-const strButton = document.getElementById('start');
-
-strButton.addEventListener('click', function(){
-
-    if (document.getElementById('diff').value == "easy") {
-        create();
-    }
-
-    else if (document.getElementById('diff').value == "normal") {
-        createNormal();
-    }
-
-
-    else if (document.getElementById('diff').value == "hard") {
-        createHard();
-    }
-
+startButton.addEventListener('click', function(){
     
+    if(document.getElementById('diff').value == 'easy') {
+            easy();
+        }
+    
+    
+    else if (document.getElementById('diff').value == 'normal') {
+        normal();
+    }
+   
+    else if (document.getElementById('diff').value == 'hard') {
+        hard();
+    }
+    
+
 })
 
-/************
-   FUNCTIONS
-*************/
+//FUNCTIONS
 
-function create() {
-    for (i = 1; i <= 49; i++) {
-        const newCell = document.createElement('div');
-        newCell.classList.add('square', 'square-7');
-        myCont.append(newCell);
-        newCell.innerHTML = i;
-        newCell.addEventListener('click', function(){
-            this.classList.toggle('active')
-            console.log(this.innerHTML)
+function easy() {
+    for(i = 1; i <= 49; i++) {
+        const newDiv = document.createElement('div');
+        container.append(newDiv);
+        newDiv.append(i)
+        newDiv.classList.add('square', 'square-7')
+        newDiv.addEventListener('click', function(){
+            this.classList.toggle('active');
+            console.log(this.innerHTML);
         })
     }
 }
 
-function createNormal() {
-    for (i = 1; i <= 81; i++) {
-        const newCell = document.createElement('div');
-        newCell.classList.add('square', 'square-9');
-        myCont.append(newCell);
-        newCell.innerHTML = i;
-        newCell.addEventListener('click', function(){
-            this.classList.toggle('active')
-            console.log(this.innerHTML)
+function normal() {
+    for(i = 1; i <= 81; i++) {
+        const newDiv = document.createElement('div');
+        container.append(newDiv);
+        newDiv.append(i)
+        newDiv.classList.add('square', 'square-9')
+        newDiv.addEventListener('click', function(){
+            this.classList.toggle('active');
+            console.log(this.innerHTML);
         })
     }
 }
 
-function createHard() {
-    for (i = 1; i <= 100; i++) {
-        const newCell = document.createElement('div');
-        newCell.classList.add('square', 'square-10');
-        myCont.append(newCell);
-        newCell.innerHTML = i;
-        newCell.addEventListener('click', function(){
-            this.classList.toggle('active')
-            console.log(this.innerHTML)
+function hard() {
+    for(i = 1; i <= 100; i++) {
+        const newDiv = document.createElement('div');
+        container.append(newDiv);
+        newDiv.append(i)
+        newDiv.classList.add('square', 'square-10')
+        newDiv.addEventListener('click', function(){
+            this.classList.toggle('active');
+            console.log(this.innerHTML);
         })
     }
 }
