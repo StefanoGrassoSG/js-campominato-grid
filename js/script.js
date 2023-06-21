ed0aee52194ec5b7daee18c11d1ce72956c61d3f
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-21 17:01:56 +0200

    EDIT functions in js

--- js/script.js
+++ js/script.js
@@ -1,56 +1,64 @@
 
 const myCont = document.querySelector('.container')
-const  diffList = document.getElementById('diff').value;
 const strButton = document.getElementById('start');
 
 strButton.addEventListener('click', function(){
 
     if (document.getElementById('diff').value == "easy") {
-        for (i = 1; i <= 49; i++) {
-            const newCell = document.createElement('div');
-            newCell.classList.add('square', 'square-10');
-            myCont.append(newCell);
-            newCell.innerHTML = i;
-            newCell.addEventListener('click', function(){
-                this.classList.toggle('active')
-                console.log(this.innerHTML)
-            })
-        }
-    }
-
-
-    if (document.getElementById('diff').value == "easy") {
-        for (i = 1; i <= 100; i++) {
-            const newCell = document.createElement('div');
-            newCell.classList.add('square', 'square-10');
-            myCont.append(newCell);
-            newCell.innerHTML = i;
-            newCell.addEventListener('click', function(){
-                this.classList.toggle('active')
-                console.log(this.innerHTML)
-            })
-        }
+        create();
     }
 
     else if (document.getElementById('diff').value == "normal") {
-        for (i = 1; i <= 81; i++) {
-            const newCell = document.createElement('div');
-            newCell.classList.add('square', 'square-9');
-            myCont.append(newCell);
-            newCell.innerHTML = i;
-            newCell.addEventListener('click', function(){
-                this.classList.toggle('active')
-                console.log(this.innerHTML)
-            })
-        }
+        createNormal();
     }
+
+
+    else if (document.getElementById('diff').value == "hard") {
+        createHard();
+    }
+
+    
 })
-
-
-
-
 
 /************
    FUNCTIONS
 *************/
 
+function create() {
+    for (i = 1; i <= 49; i++) {
+        const newCell = document.createElement('div');
+        newCell.classList.add('square', 'square-7');
+        myCont.append(newCell);
+        newCell.innerHTML = i;
+        newCell.addEventListener('click', function(){
+            this.classList.toggle('active')
+            console.log(this.innerHTML)
+        })
+    }
+}
+
+function createNormal() {
+    for (i = 1; i <= 81; i++) {
+        const newCell = document.createElement('div');
+        newCell.classList.add('square', 'square-9');
+        myCont.append(newCell);
+        newCell.innerHTML = i;
+        newCell.addEventListener('click', function(){
+            this.classList.toggle('active')
+            console.log(this.innerHTML)
+        })
+    }
+}
+
+function createHard() {
+    for (i = 1; i <= 100; i++) {
+        const newCell = document.createElement('div');
+        newCell.classList.add('square', 'square-10');
+        myCont.append(newCell);
+        newCell.innerHTML = i;
+        newCell.addEventListener('click', function(){
+            this.classList.toggle('active')
+            console.log(this.innerHTML)
+        })
+    }
+}
