f5a4ceb3884127fdc9c00a55efdb21516441e3b9
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-21 16:17:10 +0200

    ADD different diffyculties in js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,52 @@
 
 const myCont = document.querySelector('.container')
+const  diffList = document.getElementById('diff').value;
+const strButton = document.getElementById('start');
 
-for (i = 1; i <= 100; i++) {
-    const newCell = document.createElement('div');
-    newCell.classList.add('square');
-    myCont.append(newCell);
-    newCell.innerHTML = i;
-    newCell.addEventListener('click', function(){
-        this.classList.toggle('active')
-        console.log(this.innerHTML)
-    })
-    
+strButton.addEventListener('click', function(){
 
-}
+    if (document.getElementById('diff').value == "easy") {
+        for (i = 1; i <= 49; i++) {
+            const newCell = document.createElement('div');
+            newCell.classList.add('square', 'square-10');
+            myCont.append(newCell);
+            newCell.innerHTML = i;
+            newCell.addEventListener('click', function(){
+                this.classList.toggle('active')
+                console.log(this.innerHTML)
+            })
+        }
+    }
+
+
+    if (document.getElementById('diff').value == "easy") {
+        for (i = 1; i <= 100; i++) {
+            const newCell = document.createElement('div');
+            newCell.classList.add('square', 'square-10');
+            myCont.append(newCell);
+            newCell.innerHTML = i;
+            newCell.addEventListener('click', function(){
+                this.classList.toggle('active')
+                console.log(this.innerHTML)
+            })
+        }
+    }
+
+    else if (document.getElementById('diff').value == "normal") {
+        for (i = 1; i <= 81; i++) {
+            const newCell = document.createElement('div');
+            newCell.classList.add('square', 'square-9');
+            myCont.append(newCell);
+            newCell.innerHTML = i;
+            newCell.addEventListener('click', function(){
+                this.classList.toggle('active')
+                console.log(this.innerHTML)
+            })
+        }
+    }
+})
+
+
 
 
 
