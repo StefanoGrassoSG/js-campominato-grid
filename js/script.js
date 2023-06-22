4e90666824fdeba834e75b846f5ba2a001a6cc13
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-06-22 11:06:18 +0200

    EDIT functions in js

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ const startButton = document.getElementById('start')
 const container = document.querySelector('.container-game')
 
 startButton.addEventListener('click', function(){
-    
+    container.innerHTML = '';
     if(document.getElementById('diff').value == 'easy') {
-            easy();
+        createMyElement(49, 'square-7');
         }
     
     
     else if (document.getElementById('diff').value == 'normal') {
-        normal();
+        createMyElement(81, 'square-9');
     }
    
     else if (document.getElementById('diff').value == 'hard') {
-        hard();
+        createMyElement(100, 'square-10');
     }
     
 
@@ -21,38 +21,12 @@ startButton.addEventListener('click', function(){
 
 //FUNCTIONS
 
-function easy() {
-    for(i = 1; i <= 49; i++) {
+function createMyElement(cellNumber, classes) {
+    for(i = 1; i <= cellNumber; i++) {
         const newDiv = document.createElement('div');
         container.append(newDiv);
         newDiv.append(i)
-        newDiv.classList.add('square', 'square-7')
-        newDiv.addEventListener('click', function(){
-            this.classList.toggle('active');
-            console.log(this.innerHTML);
-        })
-    }
-}
-
-function normal() {
-    for(i = 1; i <= 81; i++) {
-        const newDiv = document.createElement('div');
-        container.append(newDiv);
-        newDiv.append(i)
-        newDiv.classList.add('square', 'square-9')
-        newDiv.addEventListener('click', function(){
-            this.classList.toggle('active');
-            console.log(this.innerHTML);
-        })
-    }
-}
-
-function hard() {
-    for(i = 1; i <= 100; i++) {
-        const newDiv = document.createElement('div');
-        container.append(newDiv);
-        newDiv.append(i)
-        newDiv.classList.add('square', 'square-10')
+        newDiv.classList.add('square', classes)
         newDiv.addEventListener('click', function(){
             this.classList.toggle('active');
             console.log(this.innerHTML);
